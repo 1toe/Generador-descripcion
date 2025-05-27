ce072c8a697047ed6ff2fa8881d1b2803fadb8ac
1toe <gnarforall@gmail.com>
2025-05-27 00:52:33 -0400

    chore: bruh

--- backend/supabase.js
+++ backend/supabase.js
@@ -1,3 +1,3 @@
 const { createClient } = supabase
-const _supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+const _supabase = createClient('https://vwimoqgolsovjpnkmmpy.supabase.co', 'AVFDmGhWhf2woGmBL4Nv83ll4BlDShzsfIs9gA4IFo9SaP0Zboi6knfOSZFW')
 console.log('Cliente de Supabase creado')
